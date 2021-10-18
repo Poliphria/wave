2f4318ae9ae5f6f601273c918321917695b8d6a2
Christian Moronta <cmoronta26@gmail.com>
2021-10-18 05:16:48 -0400

    Initial commit/Make Gradient Background

---
+++ pages/components/Container.js
@@ -0,0 +1,54 @@
+import {
+    Flex,
+    keyframes,
+    usePrefersReducedMotion
+} from '@chakra-ui/react'
+
+const gradient = keyframes`
+      0% { background-position: 0% }
+      100% { background-position: 100% }
+      `;
+
+export const Container = (props) => {
+    const prefersReducedMotion = usePrefersReducedMotion()
+    const gradientAnimation = prefersReducedMotion ? undefined : `${gradient} 9s alternate infinite`
+
+    return (
+        <Flex
+            direction="column"
+            alignItems="center"
+            justifyContent="flex-start"
+            bgGradient="linear(to-r, #F1354C, #B32189, #9A2D53)"
+            position="absolute"
+            // bgColor="blackAlpha.100"
+            bgSize="300% 300%"
+            width="100vw"
+            height="100vh"
+            animation={gradientAnimation}
+            {...props}
+        />
+    )
+}
+
+
+    // <style>
+    // .background--custom {
+    //   background: linear-gradient(90deg, #F1354C, #B32189, #9A2D53);
+    //   background-size: 300% 300%;
+    //   animation: gradient 9s alternate infinite;
+    //   position: absolute;
+    //   top: 0;
+    //   left: 0;
+    //   height: 100%;
+    //   width: 100%;
+    // }
+    // @keyframes gradient {
+    //   0% {
+    //     background-position: 0%;
+    //   }
+    //   100% {
+    //     background-position: 100%;
+    //   }
+    // }
+    // </style>
+
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Container } from "./components/Container"
+import { Text } from "@chakra-ui/layout"
+
+export default function Home() {
+  return (
+    <Container justifyContent="center">
+      
+    </Container>
+  )
+}
