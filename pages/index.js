71288a2a1fbef2dc53a999534637d4b633508be6
Christian Moronta <cmoronta26@gmail.com>
2021-12-30 03:06:23 -0500

    change some font stuff

--- pages/index.js
+++ pages/index.js
@@ -6,30 +6,33 @@ import { Container } from "./components/Container"
 import { StackDivider, Text, VStack } from "@chakra-ui/layout"
 import { Button } from "@chakra-ui/button"
 import { Input } from "@chakra-ui/input"
-
+import "@fontsource/rubik"
 export default function Home() {
   return (
-    <Container justifyContent="center">
+    <Container justifyContent="center" fontFamily="Rubik">
       <VStack
         //divider={<StackDivider borderColor="gray.200" />}
-        spacing="12"
+        spacing="8"
         align="center"
         width="lg"
         height="lg"
         justify="center"
       >
         <Button
-          bgColor="purple.300"
+          bgGradient={`linear(to-r, #4c2a85, #bcedf6)`}
+          textColor="#f7f7f7" 
           borderRadius="16px"
-          minWidth="64dp"
-          height="36px"
+          height="64px"
+          width="xs"
           paddingLeft="16px"
           paddingRight="16px"
+          fontSize="x-large"
+          boxShadow="lg"
         >
           Upload
         </Button>
-        <Text>OR</Text>
-        <Input bgColor="green" placeholder="YouTube Link" variant="filled"/>
+        <Text fontSize="x-large" fontSize="xx-large">Or</Text>
+        <Input placeholder="YouTube Link" size="lg" fontSize="xl" borderWidth="2px"/>
       </VStack>
     </Container>
   )
